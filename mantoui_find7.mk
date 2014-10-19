aa6309c613b198a8722705e8440060af5cab4db8
namantou <namantou@gmail.com>
2014-10-19 10:06:02 +0800

    Mantoui

--- mantoui_find7.mk
+++ mantoui_find7.mk
@@ -23,19 +23,19 @@
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/mantoui/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/mantoui/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oppo/find7/device.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_find7
+PRODUCT_NAME := mantoui_find7
 PRODUCT_DEVICE := find7
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := FIND7
